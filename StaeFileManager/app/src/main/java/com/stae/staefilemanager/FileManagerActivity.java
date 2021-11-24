1f35f217a4654fbe3ed8724c2034013862daea46
doritmtm <dorian_tm@yahoo.com>
2021-11-24 18:54:15 +0200

    App properly asks for write permission and added folder icon

--- StaeFileManager/app/src/main/java/com/stae/staefilemanager/FileManagerActivity.java
+++ StaeFileManager/app/src/main/java/com/stae/staefilemanager/FileManagerActivity.java
@@ -1,12 +1,29 @@
 package com.stae.staefilemanager;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityOptionsCompat;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
+import android.app.Application;
+import android.app.DownloadManager;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -18,10 +35,26 @@ import java.util.ArrayList;
 public class FileManagerActivity extends AppCompatActivity {
     private RecyclerView fileRecyclerView;
     private ArrayList<FileItem> fileItemArray;
+    private ActivityResultLauncher<String> activityResultLauncher;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_file_manager);
+        activityResultLauncher=registerForActivityResult(new ActivityResultContracts.RequestPermission(),granted ->{
+            if(granted)
+            {
+                //good!! :))
+            }
+            else
+            {
+                Intent intent = new Intent();
+                intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                intent.addCategory(Intent.CATEGORY_DEFAULT);
+                intent.setData(Uri.parse("package:" + getPackageName()));
+                startActivity(intent);
+            }
+        });
+        //checkWritePermission4();
         setSupportActionBar(findViewById(R.id.toolbar));
         fileItemArray=new ArrayList<>();
         fileItemArray.add(new FileItem("file01"));
@@ -33,9 +66,133 @@ public class FileManagerActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onResume() {
+        super.onResume();
+        checkWritePermission4();
+    }
+
+    private void checkWritePermission4() {
+        if(ContextCompat.checkSelfPermission(this,Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED)
+        {
+            AlertDialog.Builder builder=new AlertDialog.Builder(this);
+            builder.setMessage("File managing permission has not been granted!\nThe permission is required for the app to be able to manage files on the device!")
+                    .setTitle("Permission required")
+                    .setPositiveButton("Go to settings", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            Intent intent = new Intent();
+                            intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                            intent.addCategory(Intent.CATEGORY_DEFAULT);
+                            intent.setData(Uri.parse("package:" + getPackageName()));
+                            startActivity(intent);
+                        }
+                    });
+            builder.create().show();
+        }
+    }
+
+    private void checkWritePermission3() {
+        if(ContextCompat.checkSelfPermission(this,Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED)
+        {
+            AlertDialog.Builder builder=new AlertDialog.Builder(this);
+            builder.setMessage("File managing permission has not been granted!\nThe permission is required for the app to be able to manage files on the device!")
+                    .setTitle("Permission required")
+                    .setPositiveButton("Go to settings", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
+                            }
+                        }
+                    });
+            builder.create().show();
+        }
+    }
+
+    private void checkWritePermission2() {
+        if(ContextCompat.checkSelfPermission(this,Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED)
+        {
+            activityResultLauncher=registerForActivityResult(new ActivityResultContracts.RequestPermission(),granted ->{
+                if(granted)
+                {
+                    //good!! :))
+                }
+                else
+                {
+                    AlertDialog.Builder builder=new AlertDialog.Builder(this);
+                    builder.setMessage("File managing permission has not been granted!\nThe permission is required for the app to be able to manage files on the device!")
+                            .setTitle("Permission required")
+                            .setPositiveButton("Go to settings", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    activityResultLauncher.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE);//PERMISSION PROBLEM: READ, WRITE OR MANAGE?????
+                                }
+                            });
+                    builder.create().show();
+                }
+            });
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if(shouldShowRequestPermissionRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE))
+                {
+                    Log.d("MYAPPPPPPPPP","rationale:TRUE");
+                }
+                else
+                {
+                    Log.d("MYAPPPPPPPPP","rationale:FALSE");
+                }
+            }
+            AlertDialog.Builder builder=new AlertDialog.Builder(this);
+            builder.setMessage("File managing permission has not been granted!\nThe permission is required for the app to be able to manage files on the device!")
+                    .setTitle("Permission required")
+                    .setPositiveButton("Go to settings", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            activityResultLauncher.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE);//PERMISSION PROBLEM: READ, WRITE OR MANAGE?????
+                        }
+                    });
+            builder.create().show();
+        }
+    }
+
+    private void checkWritePermission() {
+        if(ContextCompat.checkSelfPermission(this,Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED)
+        {
+            activityResultLauncher=registerForActivityResult(new ActivityResultContracts.RequestPermission(),granted ->{
+                if(granted)
+                {
+                    //good!! :))
+                }
+                else
+                {
+                    AlertDialog.Builder builder=new AlertDialog.Builder(this);
+                    builder.setMessage("File managing permission has not been granted!\nThe permission is required for the app to be able to manage files on the device!")
+                            .setTitle("Permission required")
+                            .setPositiveButton("Go to settings", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    activityResultLauncher.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE);//PERMISSION PROBLEM: READ, WRITE OR MANAGE?????
+                                }
+                            });
+                    builder.create().show();
+                }
+            });
+            AlertDialog.Builder builder=new AlertDialog.Builder(this);
+            builder.setMessage("File managing permission has not been granted!\nThe permission is required for the app to be able to manage files on the device!")
+                    .setTitle("Permission required")
+                    .setPositiveButton("Go to settings", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            activityResultLauncher.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE);//PERMISSION PROBLEM: READ, WRITE OR MANAGE?????
+                        }
+                    });
+            builder.create().show();
+        }
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.toolbar_menu,menu);
-        return true;
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
@@ -47,6 +204,6 @@ public class FileManagerActivity extends AppCompatActivity {
                 startActivity(intent);
                 break;
         }
-        return true;
+        return super.onOptionsItemSelected(item);
     }
 }
