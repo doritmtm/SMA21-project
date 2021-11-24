1262245573e4da4376a72cf4858eb4d5b04f1691
doritmtm <dorian_tm@yahoo.com>
2021-11-24 22:20:56 +0200

    App now shows the contents of /sdcard/

--- StaeFileManager/app/src/main/java/com/stae/staefilemanager/SettingsActivity.java
+++ StaeFileManager/app/src/main/java/com/stae/staefilemanager/SettingsActivity.java
@@ -2,18 +2,50 @@ package com.stae.staefilemanager;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
+import androidx.core.widget.NestedScrollView;
 
 import android.Manifest;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Switch;
 
 public class SettingsActivity extends AppCompatActivity {
+    private SwitchCompat nightSwitch,systemNightSwitch;
+    private NestedScrollView settingsScroll;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        setSupportActionBar(findViewById(R.id.toolbar2));
+        nightSwitch=findViewById(R.id.nightSwitch);
+        systemNightSwitch=findViewById(R.id.systemNightSwitch);
+        nightSwitch.setChecked(true);
+        settingsScroll=findViewById(R.id.settingsScroll);
+        settingsScroll.post(() -> settingsScroll.scrollTo(0,0));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId())
+        {
+            case R.id.toolbarSettings:
+                Intent intent=new Intent(this,SettingsActivity.class);
+                startActivity(intent);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
